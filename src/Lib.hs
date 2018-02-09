b166b61c9a9212fde897ead170394fa50ee5539b
Zefpe2 <Zefpe2@gmail.com>
2018-02-09 10:44:52 +0800

    complete grid

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
     (grid
     ,languages
+    ,Grid
     ,formatGrid
     ,outputGrid
     ,findWord
@@ -29,5 +30,16 @@ findWords grid words =
 findWordInLine :: String -> String ->Bool
 findWordInLine = isInfixOf
 
-grid = [ "__C________R__","__SI________U__"]
-languages = ["BASIC","COBOL","CSHARP","JAVA","HASKELL"]
+grid = [ "__C________R__"
+        ,"__SI________U__"
+        ,"__HASKELL____B_"
+        ,"__A__A_____S__Y"
+        ,"__R___B___C____"
+        ,"__PHP____H_____"
+        ,"____S_LREP_____"
+        ,"____L_E__T_O___"
+        ,"_________HB____"
+        ,"_________O_____"
+        ,"________CN_____"
+        ]
+languages = ["BASIC","COBOL","CSHARP","JAVA","HASKELL","LISP","PERL","PHP","PYTHON","RUBY","SCHEME"]
